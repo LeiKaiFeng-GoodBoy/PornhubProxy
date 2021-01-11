3c33322e492b098468dba0a6b9db496411aab4e8
leikaifeng <leikaifeng@LEIKAIFENG-PC>
2021-01-11 13:39:41 +0800

    all

--- LeiKaiFeng.Http/MyChannels.cs
+++ LeiKaiFeng.Http/MyChannels.cs
@@ -5,8 +5,18 @@ using System.Threading.Tasks;
 
 namespace LeiKaiFeng.Http
 {
+    [Serializable]
+    public sealed class MyChannelsCompletedException : Exception
+    {
+
+    }
+
     public sealed class MyChannels<T> where T : class
     {
+        
+
+        readonly CancellationTokenSource m_source = new CancellationTokenSource();
+
         readonly ConcurrentQueue<T> m_queue = new ConcurrentQueue<T>();
 
         readonly SemaphoreSlim m_write_slim;
@@ -23,31 +33,71 @@ namespace LeiKaiFeng.Http
 
         public async Task WriteAsync(T value)
         {
-            await m_write_slim.WaitAsync().ConfigureAwait(false);
+            try
+            {
+
+                await m_write_slim.WaitAsync(m_source.Token).ConfigureAwait(false);
+
+            }
+            catch (OperationCanceledException)
+            {
+                throw new MyChannelsCompletedException();
+            }
 
             m_queue.Enqueue(value);
 
-            await Task.Run(m_read_slim.Release).ConfigureAwait(false);
+            m_read_slim.Release();
         }
 
         public async Task<T> ReadAsync()
         {
-            await m_read_slim.WaitAsync().ConfigureAwait(false);
+            bool canceled;
+
+            try
+            {
+
+                await m_read_slim.WaitAsync(m_source.Token).ConfigureAwait(false);
+
+                canceled = false;
+
+            }
+            catch (OperationCanceledException)
+            {
+                canceled = true;
+            }
 
 
             if (m_queue.TryDequeue(out T value))
             {
-                await Task.Run(m_write_slim.Release).ConfigureAwait(false);
+                m_write_slim.Release();
 
                 return value;
             }
             else
             {
-                throw new NotSupportedException("内部实现出错");
+                if (canceled)
+                {
+                    throw new MyChannelsCompletedException();
+                }
+                else
+                {
+                    throw new NotSupportedException("内部实现出错");
+                }
+                
             }
 
             
         }
 
+        int m_flag = 0;
+        public void CompleteAdding()
+        {
+            if (Interlocked.Exchange(ref m_flag, 1) == 0)
+            {
+
+                m_source.Cancel();
+            }
+
+        }
     }
 }
