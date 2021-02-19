b65490d54107d034c177936e9410059c4d46f2bd
leikaifeng <leikaifeng@LEIKAIFENG-PC>
2021-02-20 07:48:26 +0800

    all

--- PornhubProxy/Program.cs
+++ PornhubProxy/Program.cs
@@ -16,6 +16,8 @@ using LeiKaiFeng.Http;
 using LeiKaiFeng.Pornhub;
 using System.Runtime.InteropServices;
 using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography;
 
 namespace PornhubProxy
 {
@@ -113,6 +115,69 @@ namespace PornhubProxy
     }
 
 
+    public static class TLSCertificate
+    {
+
+        static X509Extension CreateSubAltName(string[] subjectAltNames)
+        {
+            var builder = new SubjectAlternativeNameBuilder();
+
+            Array.ForEach(subjectAltNames, (s) => builder.AddDnsName(s));
+
+            return builder.Build(false);
+        }
+
+        static void AddExtension(Collection<X509Extension> extensions, string[] subjectAltNames)
+        {
+
+
+            extensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DataEncipherment, false));
+            extensions.Add(new X509BasicConstraintsExtension(false, true, 0, false));
+            extensions.Add(CreateSubAltName(subjectAltNames));
+        }
+
+        public static X509Certificate2 CreateTlsCertificate(string commonName, X509Certificate2 caCertificate, int keySize, int days, params string[] subjectAltNames)
+        {
+            string subjectName = $"CN = {commonName}";
+
+            var rsa = RSA.Create(keySize);
+
+            var certificateRequest = new CertificateRequest(subjectName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+            AddExtension(certificateRequest.CertificateExtensions, subjectAltNames);
+
+
+
+            var dateTime = DateTime.UtcNow;
+
+
+
+            X509Certificate2 tlsCertificate = certificateRequest.Create(caCertificate, new DateTimeOffset(dateTime), new DateTimeOffset(dateTime.AddDays(days)), caCertificate.GetCertHash().Take(20).ToArray());
+
+            return new X509Certificate2(tlsCertificate.CopyWithPrivateKey(rsa).Export(X509ContentType.Pfx));
+        }
+
+
+        public static X509Certificate2 CreateCA(string commonName, int keySize, int days)
+        {
+            string subjectName = $"CN = {commonName}";
+
+            var rsa = RSA.Create(keySize);
+
+
+
+            var certificateRequest = new CertificateRequest(subjectName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+            certificateRequest.CertificateExtensions.Add(new X509BasicConstraintsExtension(true, true, 1, true));
+
+            var dateTime = DateTime.UtcNow;
+
+            return certificateRequest.CreateSelfSigned(new DateTimeOffset(dateTime), new DateTimeOffset(dateTime.AddDays(days)));
+
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -127,17 +192,22 @@ namespace PornhubProxy
 
             SetProxy.Set(pacServer.ProxyUri);
 
+            X509Certificate2 ca = new X509Certificate2("myCA.pfx");
+
+            X509Certificate2 mainCert = TLSCertificate.CreateTlsCertificate("pornhub.com", ca, 2048, 2, "pornhub.com", "*.pornhub.com");
+            X509Certificate2 adCert = TLSCertificate.CreateTlsCertificate("adtng.com", ca, 2048, 2, "adtng.com", "*.adtng.com");
+
             PornhubProxyInfo info = new PornhubProxyInfo
             {
-                MainPageStreamCreate = Connect.CreateLocalStream(new X509Certificate2(File.ReadAllBytes("main.com.pfx"))),
+                MainPageStreamCreate = Connect.CreateLocalStream(new X509Certificate2(mainCert)),
 
-                ADPageStreamCreate = Connect.CreateLocalStream(new X509Certificate2(File.ReadAllBytes("adtng.com.pfx"))),
+                ADPageStreamCreate = Connect.CreateLocalStream(new X509Certificate2(adCert)),
 
                 RemoteStreamCreate = Connect.CreateRemoteStream,
 
                 MaxContentSize = 1024 * 1024 * 5,
 
-                ADVideoBytes = File.ReadAllBytes("985106_video_with_sound.mp4"),
+                ADVideoBytes = File.ReadAllBytes("ad.mp4"),
 
                 CheckingVideoHtml = Connect.CheckingVideoHtml,
 
